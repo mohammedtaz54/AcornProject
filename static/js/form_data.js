364aa559febaf5a7e4edfa19689bfbdac4ce8bab
unknown <christianr@cardiff.ac.uk>
2017-11-27 18:48:59 +0000

    Added js to ensure that both email fields are the same

---
+++ static/js/form_data.js
@@ -0,0 +1,7 @@
+// function confirmEmail() {
+//   var email = document.getElementById("eAddress").value
+//   var confemail = document.getElementById("confirmEAddress").value
+//   if(email != confemail) {
+//     alert('Email Not Matching!');
+//   }
+// }
