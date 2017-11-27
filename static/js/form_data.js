bd175d1e6ccb11263788675652e414e5c69d698f
unknown <christianr@cardiff.ac.uk>
2017-11-27 20:34:21 +0000

    Conner's work that I accidentally removed

--- static/js/form_data.js
+++ static/js/form_data.js
@@ -3,5 +3,11 @@ function confirmEmail() {
   var confemail = document.forms["contractorForm"]['confirmEAddress'].value;
   if(email != confemail) {
     alert('Email Not Matching!');
+    email.style.borderColor="red";
+    confemail.sytle.borderColor="red";
+  } else{
+    email.style.borderColor="rgb(112,111,111)";
+    confemail.sytle.borderColor="rgb(112,111,111)";
   }
+
 }
