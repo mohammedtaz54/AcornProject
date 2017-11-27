337aa4165f5dc0f44ae552f14f8947bc13b35db6
Ryan Christian <christianr@cardiff.ac.uk>
2017-11-27 14:05:23 +0000

    Random cleaning

--- Server.py
+++ Server.py
@@ -26,12 +26,6 @@ def addContractorDetails():
         # title = request.form.get('title', default="Error")
         print("inserting contractor "+valuelist[1])
         try:
-            # for i in range(0, 38):
-            #     conn = sqlite3.connect(DATABASE)
-            #     cur = conn.cursor()
-            #     cur.execute("INSERT INTO form_data (fieldlist[i])\
-            #                 VALUES (?)", (valuelist[i]) )
-            #     conn.commit()
             conn = sqlite3.connect(DATABASE)
             cur = conn.cursor()
             cur.execute("INSERT INTO form_data ('title', 'firstName', 'surname', 'gender', 'dob', 'niNumber', 'eAddress', 'contactNumber','postCode', 'addressLine1', 'addressLine2', 'addressLine3', 'town', 'emergContact','emergContactNumber', 'workReq', 'quali', 'nameOfCompany', 'eligibility',\
@@ -39,15 +33,7 @@ def addContractorDetails():
                         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (valuelist[0],valuelist[1],valuelist[2],valuelist[3],valuelist[4],valuelist[5],valuelist[6],valuelist[7],valuelist[8],valuelist[9],valuelist[10],valuelist[11],valuelist[12],valuelist[13],\
                             valuelist[14],valuelist[15],valuelist[16],valuelist[17],valuelist[18],valuelist[19],valuelist[20],valuelist[21],valuelist[22],valuelist[23],valuelist[24],valuelist[25],valuelist[26],valuelist[27],valuelist[28],valuelist[29],valuelist[30],valuelist[31],valuelist[32],valuelist[33],\
                             valuelist[34],valuelist[35],valuelist[36]) )
-            print("first gucci")
             conn.commit()
-            #
-            # conn = sqlite3.connect(DATABASE)
-            # cur = conn.cursor()
-            # cur.execute("INSERT INTO form_data ('proofOfEligibility', 'licence', 'criminalConviction', 'criminalDetails', 'disability','disabilityDetails', 'refereeName1', 'refereeJob1', 'refereeComp1', 'refereeAddress1','refereeNum1','refereeEmail1', 'refereeName2', 'refereeJob2', 'refereeComp2','refereeAddress2','refereeNum2','refereeEmail2')\
-            #             VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (valuelist[19],valuelist[20],valuelist[21],valuelist[22],valuelist[23],valuelist[24],valuelist[25],valuelist[26],valuelist[27],valuelist[28],valuelist[29],valuelist[30],valuelist[31],valuelist[32],valuelist[33],valuelist[34],valuelist[35],valuelist[36]) )
-            # print("Second gucci")
-            # conn.commit()
             msg = "Record sucessfully added"
         except:
             conn.rollback()
