d35e97ad5c7e50c2ebde35af2edb7506de62b1a3
unknown <christianr@cardiff.ac.uk>
2017-11-27 22:07:40 +0000

    Fixed the last of the merge issues

--- static/js/form_data.js
+++ static/js/form_data.js
@@ -1,49 +1,4 @@
 
-function eValidate(){
-  var email = document.getElementById('eAddress')
-  var emailC = document.getElementById('confirmEAddress')
-  if(email.value != emailC.value){
-    email.style.borderColor = "red";
-    emailC.style.borderColor = "red";
-  }
-  else{
-    email.style.borderColor = "rgb(112,111,111)";
-    emailC.style.borderColor = "rgb(112,111,111)";
-  }
-}
-
-function hide(){
-  var removeIt = document.getElementById('removed');
-  var removeIt1 = document.getElementById('removed1');
-  var removeIt2 = document.getElementById('removed2');
-  var option = document.forms["contractorForm"]["eligibility"].value
-  var option1 = document.forms["contractorForm"]["criminalConviction"].value
-  var option2 = document.forms["contractorForm"]["disability"].value
-
-if(option == "No"){
-    removeIt.style.visibility = "hidden";
-
-}
-else if(option == "Yes"){
-  removeIt.style.visibility = "visible";
-
-}
-
-if(option1 == "No"){
-    removeIt1.style.visibility = "hidden";
-}
-else if(option1 == "Yes"){
-  removeIt1.style.visibility = "visible";
-}
-
-if(option2 == "No"){
-    removeIt2.style.visibility = "hidden";
-}
-else if(option2 == "Yes"){
-  removeIt2.style.visibility = "visible";
-}
-}
-
 function confirmEmail() {
   var email = document.forms["contractorForm"]['eAddress'].value;
   var confemail = document.forms["contractorForm"]['confirmEAddress'].value;
@@ -56,4 +11,36 @@ function confirmEmail() {
     confemail.sytle.borderColor="rgb(112,111,111)";
   }
 
+}
+
+function hide(){
+  var removeIt = document.getElementById('removed');
+  var removeIt1 = document.getElementById('removed1');
+  var removeIt2 = document.getElementById('removed2');
+  var option = document.forms["contractorForm"]["eligibility"].value
+  var option1 = document.forms["contractorForm"]["criminalConviction"].value
+  var option2 = document.forms["contractorForm"]["disability"].value
+
+  if(option == "No"){
+      removeIt.style.visibility = "hidden";
+
+  }
+  else if(option == "Yes"){
+    removeIt.style.visibility = "visible";
+
+  }
+
+  if(option1 == "No"){
+      removeIt1.style.visibility = "hidden";
+  }
+  else if(option1 == "Yes"){
+    removeIt1.style.visibility = "visible";
+  }
+
+  if(option2 == "No"){
+      removeIt2.style.visibility = "hidden";
+  }
+  else if(option2 == "Yes"){
+    removeIt2.style.visibility = "visible";
+  }
 }
