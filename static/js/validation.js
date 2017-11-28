b79d819568ba63b23b7bae7b7967c9797f2a6a5e
unknown <christianr@cardiff.ac.uk>
2017-11-28 17:53:28 +0000

    General cleaning of main html file, moved some script to js folder, and removed unused script

---
+++ static/js/validation.js
@@ -0,0 +1,156 @@
+//function addForm() {
+  // var First_name = document.forms["myForm"]["firstName"].value;
+  // var S_name = document.forms["myForm"]["surname"].value;
+  // var Date_of_birth = document.forms["myForm"]["dob"].value;
+  // var NI_Number = document.forms["myForm"]["niNumber"].value;
+  // var Email_Address = document.forms["myForm"]["eAddress"].value;
+  // var Confirm_Email_Address = document.forms["myForm"]["confirmEAddress"].value;
+  // var Contact_Number = document.forms["myForm"]["contactNumber"].value;
+  // var Postal_Code = document.forms["myForm"]["postCode"].value;
+  // var Address_Line1 = document.forms["myForm"]["addressLine1"].value;
+  // var Address_Line2 = document.forms["myForm"]["addressLine2"].value;
+  // var Address_Line13 = document.forms["myForm"]["addressLine3"].value;
+  // var Town = document.forms["myForm"]["town"].value;
+  // var Emergency_Contact_Name = document.forms["myForm"]["emergContact"].value;
+  // var Emergency_Contact_Number = document.forms["myForm"]["emergContactNumber"].value;
+  // var Work_Required = document.forms["myForm"]["workReq"].value;
+  // var Qualifications = document.forms["myForm"]["quali"].value;
+  // var Company_Name = document.forms["myForm"]["nameOfCompany"].value
+  //
+  // var Ref_name1 = document.forms["myForm"]["refereeName1"].value;
+  // var Ref_job1 = document.forms["myForm"]["refereeJob1"].value;
+  // var Ref_comp1 = document.forms["myForm"]["refereeComp1"].value;
+  // var Ref_address1 = document.forms["myForm"]["refereeAddress1"].value;
+  // var Ref_num1 = document.forms["myForm"]["refereeNum1"].value;
+  // var Ref_email1 = document.forms["myForm"]["refereeEmail1"].value;
+  // var Ref_name2 = document.forms["myForm"]["refereeName2"].value;
+  // var Ref_job2 = document.forms["myForm"]["refereeJob2"].value;
+  // var Ref_comp2 = document.forms["myForm"]["refereeComp2"].value;
+  // var Ref_address2 = document.forms["myForm"]["refereeAddress2"].value;
+  // var Ref_num2 = document.forms["myForm"]["refereeNum2"].value;
+  // var Ref_email2 = document.forms["myForm"]["refereeEmail2"].value;
+  //
+  //
+  //
+  //
+  //       if (First_name == "") {
+  //           alert("First name must be filled out");
+  //           }
+  //
+  //       if (S_name == "") {
+  //           alert("Surname must be filled out");
+  //           }
+  //
+  //       if (Date_of_birth == "") {
+  //         alert("Date of birth must be filled out");
+  //         }
+  //
+  //       if (NI_Number == "") {
+  //           alert("National insurance number must be filled out");
+  //           }
+  //
+  //       if (Email_Address == "") {
+  //           alert("Email address must be filled out");
+  //           }
+  //
+  //       if (Confirm_Email_Address == "") {
+  //           alert("Email address confirmation must be filled out");
+  //           }
+  //
+  //       if (Contact_Number == "") {
+  //           alert("Contact number must be filled out");
+  //           }
+  //
+  //       if (Postal_Code == "") {
+  //           alert("Postal code must be filled out");
+  //           }
+  //
+  //       if (Address_Line1 == "") {
+  //           alert("Address line 1 must be filled out");
+  //           }
+  //
+  //       if (Address_Line2 == "") {
+  //           alert("Address line 2 must be filled out");
+  //           }
+  //
+  //       if (Address_Line13 == "") {
+  //           alert("Address line 3 must be filled out");
+  //           }
+  //
+  //       if (Town == "") {
+  //           alert("Town name must be filled out");
+  //           }
+  //
+  //       if (Emergency_Contact_Name == "") {
+  //           alert("Emergency contact name must be filled out");
+  //           }
+  //
+  //       if (Emergency_Contact_Number == "") {
+  //           alert("Emergency contact number must be filled out");
+  //           }
+  //
+  //       if (Work_Required == "") {
+  //           alert("Work required must be filled out");
+  //           }
+  //
+  //       if (Qualifications == "") {
+  //           alert("Qualifications/licences must be filled out");
+  //           }
+  //
+  //       if (Company_Name == "") {
+  //           alert("Name of Company must be filled out");
+  //           }
+  //
+  //
+  //       if (Ref_name1 == "") {
+  //           alert("Name of referee 1 must be filled out");
+  //           }
+  //
+  //       if (Ref_job1 == "") {
+  //           alert("Job title of referee 1 must be filled out");
+  //           }
+  //
+  //       if (Ref_comp1 == "") {
+  //           alert("Company name of referee 1 must be filled out");
+  //           }
+  //
+  //       if (Ref_address1 == "") {
+  //           alert("Address of referee 1 must be filled out");
+  //           }
+  //
+  //       if (Ref_num1 == "") {
+  //           alert("Conatct number of referee 1 must be filled out");
+  //           }
+  //
+  //       if (Ref_email1 == "") {
+  //           alert("Email address of referee 1 must be filled out");
+  //           }
+  //
+  //
+  //       if (Ref_name2 == "") {
+  //           alert("Name of referee 2 must be filled out");
+  //           }
+  //
+  //       if (Ref_job2 == "") {
+  //           alert("Job title of referee 2 must be filled out");
+  //           }
+  //
+  //       if (Ref_comp2 == "") {
+  //           alert("Company name of referee 2 must be filled out");
+  //           }
+  //
+  //       if (Ref_address2 == "") {
+  //           alert("Address of referee 2 must be filled out");
+  //           }
+  //
+  //       if (Ref_num2 == "") {
+  //           alert("Conatct number of referee 2 must be filled out");
+  //           }
+  //
+  //       if (Ref_email2 == "") {
+  //           alert("Email address of referee 2 must be filled out");
+  //           }
+//
+//
+//       return false;
+//       }
