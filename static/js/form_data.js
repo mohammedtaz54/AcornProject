e553282c81dac1c342b068eb9749ca7fd0d298b3
unknown <christianr@cardiff.ac.uk>
2017-11-29 20:18:08 +0000

    Added paragraph that explained signature

--- static/js/form_data.js
+++ static/js/form_data.js
@@ -1,16 +1,27 @@
-
 function confirmEmail() {
   var email = document.forms["contractorForm"]['eAddress'].value;
   var confemail = document.forms["contractorForm"]['confirmEAddress'].value;
   if(email != confemail) {
     alert('Email Not Matching!');
     email.style.borderColor="red";
-    confemail.sytle.borderColor="red";
+    confemail.style.borderColor="red";
   } else{
     email.style.borderColor="rgb(112,111,111)";
-    confemail.sytle.borderColor="rgb(112,111,111)";
+    confemail.style.borderColor="rgb(112,111,111)";
   }
+}
 
+function agreement() {
+  if (document.getElementById("checkBox").checked == true){
+    var date = new Date();
+    var timestamp = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
+    var firstname = document.forms["contractorForm"]['firstName'].value;
+    var surname = document.forms["contractorForm"]['surname'].value;
+    document.getElementById("signature").innerHTML= firstname+" "+surname+" "+timestamp;
+  }
+  else {
+    document.getElementById("signature").innerHTML = " ";
+  }
 }
 
 function hide(){
@@ -23,20 +34,16 @@ function hide(){
 
   if(option == "No"){
       removeIt.style.visibility = "hidden";
-
   }
   else if(option == "Yes"){
     removeIt.style.visibility = "visible";
-
   }
-
   if(option1 == "No"){
       removeIt1.style.visibility = "hidden";
   }
   else if(option1 == "Yes"){
     removeIt1.style.visibility = "visible";
   }
-
   if(option2 == "No"){
       removeIt2.style.visibility = "hidden";
   }
