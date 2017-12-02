740f80fa5b37c6ed92d17851ffb6d6a9cba46a24
Conner Boast <boastc@cardiff.ac.uk>
2017-12-02 17:47:25 +0000

    Testing pregression code

---
+++ static/js/taking_steps.js
@@ -0,0 +1,19 @@
+
+var parent = document.getElementById('contractorForm');
+var hidden = document.getElementsByClassName("progress");
+var click = document.forms["contractorForm"]["niNumber"].value;
+
+for(var i = 0; hidden != 0; i++){
+hidden[i].style.display = "none"
+};
+
+function progression() {
+
+    if (hidden[0].style.display === "none") {
+        hidden[0].style.display = "block";
+    } else {
+        hidden[0].style.display = "none";
+    }
+}
+
+var option = document.forms["contractorForm"]["niNumber"].value;
